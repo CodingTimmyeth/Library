60ec3e387b069179f43de87652f8a47de8de3275
Tommy Miguel <tommymiguel@Tommys-MacBook-Pro.local>
2023-04-11 17:52:00 -0700

    Finished Project ✅

--- script.js
+++ script.js
@@ -1,10 +1,39 @@
+const btn = document.querySelector('button')
+const gridLayout = document.querySelector('.grid')
+const bookTitle = document.getElementById('title')
+const bookAuthor = document.getElementById('author')
+const bookPages = document.getElementById('pages')
 
 
 class Book {
-    constructor (title, author, pages, read) {
+    constructor (title, author, pages) {
         this.title = title;
         this.author = author
         this.pages = pages
-        this.read = read
+    }
+
+    showBook() {
+        const titleElm =  document.createElement('h1')
+        const authorElm =  document.createElement('h2')
+        const pagesElm =  document.createElement('h3')
+
+        titleElm.textContent = `Title: ${this.title}`
+        authorElm.textContent = `Author: ${this.author}`
+        pagesElm.textContent = `Pages: ${this.pages}`
+
+        const container = document.createElement('div')
+        container.appendChild(titleElm)
+        container.appendChild(authorElm)
+        container.appendChild(pagesElm)
+
+        return container
+
     }
 }
+
+btn.addEventListener('click', () => {
+    const newBook = new Book (bookTitle.value, bookAuthor.value, bookPages.value)
+
+    const div = newBook.showBook()
+    gridLayout.append(div)
+})
